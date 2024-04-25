0259dfc9d6c1e85647de72c0317fe46be4fd8997
chernyaev <Ilya.chernyaev@gorparkovki.ru>
2024-04-25 14:22:19 +0300

    KSB-1-2 Create variables count person

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_1
+
+fun main(){
+    val countOrders : Int = 75
+    val notificationPurchase : String = "Спасибо за покупку в нашем замечательном магазине"
+    println(countOrders)
+    println(notificationPurchase)
+
+    var countPerson : Int = 2000
+//    println(countPerson)
+    countPerson = 1999
+    println(countPerson)
+
+}
