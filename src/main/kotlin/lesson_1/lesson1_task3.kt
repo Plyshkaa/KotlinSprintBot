43fe8f29ff88e1f929e08ce162499f08c750f007
chernyaev <Ilya.chernyaev@gorparkovki.ru>
2024-04-25 14:35:52 +0300

    KSB-1-3 task time landing Gagarin

---
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_1
+
+fun main() {
+    val year = 1961
+    var hour = 9
+    var minut = 7
+    println(year)
+    println(hour)
+    println(minut)
+    hour = 10
+    minut = 55
+    print("$hour:$minut")
+
+}
