116cccf0475b80ff7bade3db455e46a565574e1d
chernyaev <Ilya.chernyaev@gorparkovki.ru>
2024-04-26 08:54:43 +0300

    KSB-1-2 fix format code

--- src/main/kotlin/lesson_1/lesson1_task2.kt
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -1,12 +1,12 @@
 package org.example.lesson_1
 
-fun main(){
-    val countOrders : Int = 75
-    val notificationPurchase : String = "Спасибо за покупку в нашем замечательном магазине"
+fun main() {
+    val countOrders: Int = 75
+    val notificationPurchase: String = "Спасибо за покупку в нашем замечательном магазине"
     println(countOrders)
     println(notificationPurchase)
 
-    var countPerson : Int = 2000
+    var countPerson: Int = 2000
 //    println(countPerson)
     countPerson = 1999
     println(countPerson)
