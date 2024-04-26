9a6826d0d19b0f0e1e26f5d74fc0f61b1103399a
chernyaev <Ilya.chernyaev@gorparkovki.ru>
2024-04-26 09:05:43 +0300

    KSB-1-3 fix format code

--- src/main/kotlin/lesson1/lesson1_task4.kt
+++ src/main/kotlin/lesson1/lesson1_task4.kt
@@ -2,12 +2,12 @@ package org.example.lesson1
 
 fun main() {
 
-    val length : Long = 40868600000
-    val age : Int = 27
-    val partOfDay : Double = 0.075
-    val seconds : Short = 6480
-    val partOfYear : Double = 2.0547945205479453E-4
-    val apogee : Int = 327000
+    val length: Long = 40868600000
+    val age: Int = 27
+    val partOfDay: Double = 0.075
+    val seconds: Short = 6480
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: Int = 327000
 
     println(length)
     println(age)
