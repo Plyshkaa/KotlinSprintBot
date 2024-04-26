5b95732ad88de7aa4cf50b6434439953fad2fafe
chernyaev <Ilya.chernyaev@gorparkovki.ru>
2024-04-26 08:58:12 +0300

    KSB-1-3 fix type variables

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -2,13 +2,13 @@ package org.example.lesson_1
 
 fun main() {
     val year = 1961
-    var hour = 9
-    var minut = 7
+    var hour: String = "09"
+    var minut: String = "07"
     println(year)
     println(hour)
     println(minut)
-    hour = 10
-    minut = 55
+    hour = "10"
+    minut = "55"
     print("$hour:$minut")
 
 }
