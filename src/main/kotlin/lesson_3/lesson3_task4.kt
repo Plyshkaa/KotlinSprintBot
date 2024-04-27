938cb2d3ef3cf4d4852bc962011619b8ad497e08
chernyaev <Ilya.chernyaev@gorparkovki.ru>
2024-04-27 11:46:49 +0300

    KSB-3-4 task chess

---
+++ src/main/kotlin/lesson_3/lesson3_task4.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_3
+
+fun main() {
+    var initialPosition = "E2"
+    var finalPosition = "E4"
+    var strokeNumber = 1
+
+    println("$initialPosition -> $finalPosition , $strokeNumber")
+
+    initialPosition = "D2"
+    finalPosition = "D3"
+    strokeNumber = 2
+
+    println("$initialPosition -> $finalPosition , $strokeNumber")
+
+
+}
