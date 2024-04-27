d7ce69b5b8b82f6c6698f3dfc65a9447c7719db0
chernyaev <Ilya.chernyaev@gorparkovki.ru>
2024-04-27 11:31:35 +0300

    KSB-3-2 task full name and age

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,13 @@
+package org.example.lesson_3
+
+fun main() {
+    val ageBeforeMarriage = 20
+    val ageAfterMarriage = 22
+    val fullNameBeforeMarriage = "Андреева Татьяна Сергеевна"
+
+    val fullNameAfterMarriage = "Сидорова Татьяна Сергеевна"
+
+    println("$fullNameBeforeMarriage $ageBeforeMarriage")
+    println("$fullNameAfterMarriage $ageAfterMarriage")
+}
+
