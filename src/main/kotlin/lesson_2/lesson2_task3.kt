afa1e10d3478706df05cfa4b2a3a9b8295f49d16
chernyaev <Ilya.chernyaev@gorparkovki.ru>
2024-04-27 17:50:31 +0300

    KSB-2-3 fix logic variables

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -1,12 +1,12 @@
 package org.example.lesson_2
 
 const val trainExitHour: Int = 9
-const val trainExitMinut: Int = 39
+const val trainExitMinute: Int = 39
 const val travelTime: Int = 457
 
 fun main() {
 
-    val totalMinutesFromMidnight = trainExitHour * 60 + trainExitMinut + travelTime
+    val totalMinutesFromMidnight = trainExitHour * 60 + trainExitMinute + travelTime
     val arrivalHour = (totalMinutesFromMidnight / 60) % 24
     val arrivalMinute = totalMinutesFromMidnight % 60
 
