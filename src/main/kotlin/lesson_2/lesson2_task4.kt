e43fee723b2db5dda11df0391c53285808a77b53
chernyaev <Ilya.chernyaev@gorparkovki.ru>
2024-04-27 18:00:28 +0300

    KSB-2-4 fix buff

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -1,12 +1,14 @@
 package org.example.lesson_2
 
+const val buff = 20
+
 fun main() {
     val crystal = 7
     val iron = 11
-    val buff = 0.2
 
-    val resultIronBuff: Int = (crystal * buff).toInt()
-    val resultCrystalBuff: Int = (iron * buff).toInt()
+
+    val resultIronBuff = (crystal * buff) / 100.toDouble()
+    val resultCrystalBuff = (iron * buff) / 100.toDouble()
 
     println("Бонусные кристаллы: $resultCrystalBuff")
     println("Бонусная железная руда: $resultIronBuff")
