b3388a0430d130491f208c9ffadf833726638e36
chernyaev <Ilya.chernyaev@gorparkovki.ru>
2024-04-27 11:37:36 +0300

    KSB-3-3 task multiplication table

---
+++ src/main/kotlin/lesson_3/lesson3_task3.kt
@@ -0,0 +1,19 @@
+package org.example.lesson_3
+
+fun main() {
+    val number = 6
+    println(
+        """
+       6 x 1 = ${number * 1}
+       6 x 2 = ${number * 2}
+       6 x 3 = ${number * 3}
+       6 x 4 = ${number * 4}
+       6 x 5 = ${number * 5}
+       6 x 6 = ${number * 6}
+       6 x 7 = ${number * 7}
+       6 x 8 = ${number * 8}
+       6 x 9 = ${number * 9}
+        
+    """.trimIndent()
+    )
+}
