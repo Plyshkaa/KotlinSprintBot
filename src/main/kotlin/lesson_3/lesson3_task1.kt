91a7a9e515986fb57cf1dc7cd392c165c3a8c946
chernyaev <Ilya.chernyaev@gorparkovki.ru>
2024-04-27 11:09:45 +0300

    KSB-3-1 text greeting day/night

---
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,12 @@
+package org.example.lesson_3
+
+fun main() {
+    val namePerson = "Ilya"
+    var textGreeting = "Добрый день"
+
+    println("$textGreeting $namePerson")
+
+    textGreeting = "Добрый вечер"
+    println("$textGreeting $namePerson")
+
+}
