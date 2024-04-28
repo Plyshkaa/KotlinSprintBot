9f187015eb85a86d5491b397d39562dc83edf3e8
i_chernyaev <ilya.chernyaev31@yandex.ru>
2024-04-28 12:31:33 +0300

    KSB-2-4 fix const

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -1,14 +1,15 @@
 package org.example.lesson_2
 
-const val buff = 20
+const val maxPercent = 100
 
 fun main() {
     val crystal = 7
     val iron = 11
+    val buff = 20
 
 
-    val resultIronBuff = (crystal * buff) / 100.toDouble()
-    val resultCrystalBuff = (iron * buff) / 100.toDouble()
+    val resultIronBuff = (crystal * buff) / maxPercent.toDouble()
+    val resultCrystalBuff = (iron * buff) / maxPercent.toDouble()
 
     println("Бонусные кристаллы: $resultCrystalBuff")
     println("Бонусная железная руда: $resultIronBuff")
