2b680f0ad6081242f5b1a098204045d3575d17b7
i_chernyaev <ilya.chernyaev31@yandex.ru>
2024-04-30 22:23:02 +0300

    KSB-3-5 fix split vararg

--- src/main/kotlin/lesson_3/lesson3_task5.kt
+++ src/main/kotlin/lesson_3/lesson3_task5.kt
@@ -2,11 +2,10 @@ package org.example.lesson_3
 
 fun main() {
     val playerMove = "D2-D4;0"
-    val parts = playerMove.split("-")
+    val parts = playerMove.split("-", ";")
     val initialPosition = parts[0]
-    val endAndMoveNumber = parts[1].split(";")
-    val finalPosition = endAndMoveNumber[0]
-    val moveNumber = endAndMoveNumber[1].toInt()
+    val finalPosition = parts[1]
+    val moveNumber = parts[2].toInt()
 
     println("Initial position: $initialPosition")
     println("Final position: $finalPosition")
