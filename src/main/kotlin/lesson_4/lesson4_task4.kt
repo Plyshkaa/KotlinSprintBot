93625ef64a5932c54fa8083dae58a8e23a064899
i_chernyaev <ilya.chernyaev31@yandex.ru>
2024-04-30 22:06:19 +0300

    KSB-4-4 fix trainingDay

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -2,10 +2,10 @@ package org.example.lesson_4
 
 fun main() {
     val trainingDay = 6
-    val armsAndAbs = trainingDay % 2 != 0 // Нечётные дни для рук и пресса
-    val legsAndBack = trainingDay % 2 == 0 // Чётные дни для ног и спины
+    val isEven = trainingDay % 2 == 0
 
-
+    val armsAndAbs = !isEven // Нечётные дни для рук и пресса
+    val legsAndBack = isEven // Чётные дни для ног и спины
 
 
     println(
