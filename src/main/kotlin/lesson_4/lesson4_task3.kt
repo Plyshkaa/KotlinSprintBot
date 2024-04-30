af7500579ed0ddaa948bd67cf2e109e517c97757
i_chernyaev <ilya.chernyaev31@yandex.ru>
2024-04-30 22:00:09 +0300

    KSB-4-3 fix const and new comparison

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,11 +1,15 @@
 package org.example.lesson_4
 
+const val airHumidity = 20
+const val currentYear = "Зима"
 fun main() {
     val weather = true
     val awning = true
-    val airHumidity = 20
-    val currentYear = "Зима"
-    val harvest: Boolean = weather && awning && (airHumidity == 20) && (currentYear != "Зима")
+    val currentSeason = "Зима"
+
+    val isGrowingSeason = currentSeason != currentYear
+    val isIdealHumidity = airHumidity == airHumidity
+    val harvest = weather && awning && isIdealHumidity && isGrowingSeason
 
     println("Благоприятные ли условия сейчас для роста бобовых? $harvest”")
 }
