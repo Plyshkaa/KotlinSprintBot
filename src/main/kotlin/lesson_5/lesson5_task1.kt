839ed4a59d0adf682899d5bf73655dc9d4e1800a
chernyaev <Ilya.chernyaev@gorparkovki.ru>
2024-05-02 17:18:51 +0300

    KSB-5-1 add right answer variables

--- src/main/kotlin/lesson_5/lesson5_task1.kt
+++ src/main/kotlin/lesson_5/lesson5_task1.kt
@@ -1,12 +1,11 @@
 package org.example.lesson_5
 
-const val CORRECT_ANSWER = 4
-
 fun main() {
+    val rightAnswer = 2+2
     println("Введите правильный ответ на задачу: 2 + 2 = ")
     val answer = readln().toInt()
 
-    if (answer == CORRECT_ANSWER) println("Добро пожаловать!")
+    if (answer == rightAnswer) println("Добро пожаловать!")
     else println("Доступ запрещен.")
 
 }
