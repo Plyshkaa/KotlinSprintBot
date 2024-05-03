1a45d1845706ad79c3f9022c370e74de9569259b
chernyaev <Ilya.chernyaev@gorparkovki.ru>
2024-05-03 09:00:38 +0300

    KSB-5-1 add new const

--- src/main/kotlin/lesson_4/lesson4_task5.kt
+++ src/main/kotlin/lesson_4/lesson4_task5.kt
@@ -3,6 +3,8 @@ package org.example.lesson_4
 const val MAX_COUNT_CREW = 70
 const val MIN_COUNT_CREW = 55
 const val MIN_COUNT_BOX = 50
+const val HAS_DAMAGE_SHIP = false
+const val HAS_CONDITION = true
 
 fun main() {
     print("Корабль имеет повреждения?")
@@ -17,8 +19,9 @@ fun main() {
     print("Условия благоприятные?")
     val hasConditions = readLine().toBoolean()
 
-    val flyVer1 = (!hasDamageShip && countCrew in MIN_COUNT_CREW..MAX_COUNT_CREW && countBox > MIN_COUNT_BOX) ||
-            (hasDamageShip && countCrew == MAX_COUNT_CREW && countBox >= MIN_COUNT_BOX && hasConditions)
+    val flyVer1 =
+        (hasDamageShip == HAS_DAMAGE_SHIP && countCrew in MIN_COUNT_CREW..MAX_COUNT_CREW && countBox > MIN_COUNT_BOX) && hasConditions or HAS_CONDITION ||
+                (countCrew == MAX_COUNT_CREW && countBox >= MIN_COUNT_BOX && hasConditions == HAS_CONDITION)
 
     println("Может ли корабль отправиться в плавание? $flyVer1")
 }
